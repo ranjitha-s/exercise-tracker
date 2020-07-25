6e817420336a9a955a232e2f60fd43fcfd37d48b
ranjitha-s <ranjitha210497@gmail.com>
2020-07-25 13:38:39 +0530

    exercise tracker

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true }
 );
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully"); 
-})
+
+
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
