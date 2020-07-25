8a85c75249a404c04c23e474dd590528e4fd99e9
ranjitha-s <ranjitha210497@gmail.com>
2020-07-25 12:20:26 +0530

    exercise tracker application

---
+++ client/build/precache-manifest.e2d3728e9111a01289f839c786c4631a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1bd09525b74781be139a523433fa61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c896ff757c486092a6",
+    "url": "/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "98c896ff757c486092a6",
+    "url": "/static/js/2.98f97bbc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.98f97bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884a937fa37d590fc7b5",
+    "url": "/static/js/main.1f591185.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
