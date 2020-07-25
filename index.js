22570953c8637fafa90caf7b9e95b2926ee62c3f
ranjitha-s <ranjitha210497@gmail.com>
2020-07-25 13:02:56 +0530

    exercise tracker

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
