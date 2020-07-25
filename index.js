aaa31ce84a18a18db8a143fbe074699799a42029
ranjitha-s <ranjitha210497@gmail.com>
2020-07-25 13:26:18 +0530

    exercise tracker

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+const keys = require('./config/keys');  
 
 require('dotenv').config();
 
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
